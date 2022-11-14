480d7bbc40e17facf0bdb94a47363e9647f1982a
heltonbc <heltondeveloper01@gmail.com>
2022-11-14 09:24:52 -0400

    api

--- src/data/constants.tsx
+++ src/data/constants.tsx
@@ -1,6 +1,6 @@
 const URL = "https://api.themoviedb.org/3";
 const IMAGEURL = "https://image.tmdb.org/t/p";
-const APIKEY = "";
+const APIKEY = "442c6c0255a1cb2887abf537cd81f70c";
 const APISTRING = `?api_key=${APIKEY}&language=pt-BR`;
 const EVENTS = {
     PosterClick: "PosterClick",
